13a171548426833a4cc3baf014b02d326d4a04ad
Mahit Agarwal <mahit.anuragagarwal@gmail.com>
2026-07-16 20:55:28 +0530

    Update manual overrides comments to show semester usage

--- parser_tool/manual_overrides.js
+++ parser_tool/manual_overrides.js
@@ -3,15 +3,16 @@ module.exports = {
   // Day format: 1=Mon, 2=Tue, 3=Wed, 4=Thu, 5=Fri, 6=Sat
   // Start format: 9=9AM, 10=10AM, 13=1PM, etc.
   deletions: [
-    // Example: Delete A6's Monday 9 AM class
-    // { batch: "A6", day: 1, start: 9 } 
+    // Example: Delete A6's Monday 9 AM class ONLY for semester 1
+    // { batch: "A6", day: 1, start: 9, semester: 1 } 
 
   ],
 
   // 2. CLASSES TO ADD (Format exactly like your data.js output)
   additions: [
-    /* Example: Add a new custom class for A6
+    /* Example: Add a new custom class for A6 in Semester 1
     {
+        semester: 1,
         batch: "A6",
         day: 1, 
         start: 9, 
