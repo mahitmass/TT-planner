aacfa76c670d6c6961891b73008670cec314c998
mahitmass <mahit.anuragagarwal@gmail.com>
2026-07-24 09:31:25 +0530

    Update dictionaries.js

--- parser_tool/dictionaries.js
+++ parser_tool/dictionaries.js
@@ -2,6 +2,7 @@ module.exports = {
     teachers62: {
     "LKK": "Lakhveer Kaur",
     "AMB": "Amita Bhagat",
+    "SPT": "Surbhi Tomar",
     "KKS": "Kamlesh Kumar Shukla",
     "NEA": "Neha Ahlawat",
     "ANK": "Dr. Arpita Nayek",
